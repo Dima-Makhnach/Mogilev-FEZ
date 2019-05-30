9cb792904b908a2f99619e450e30d90c6dbae0fb
Dima-Makhnach <dmakhnach@yandex.ru>
2019-05-30 08:15:34 +0300

    Pre build

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -20,4 +20,58 @@ jQuery(document).ready(function($) {
     nav: true,
     dotsEach: true,
   });
+
+  // Land Gallery
+  $('.land-gallery__list').owlCarousel({
+    items: 1,
+    margin: 5,
+    loop: true,
+    nav: true,
+    dots: false,
+  });
+
+  // Investors
+  $('.investors__list').owlCarousel({
+    items: 1,
+    loop: true,
+    center: true,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      1290: {
+        items: 4,
+        center: false
+      }
+    }
+  });
+
+  // Territory
+  $('.territory__list').owlCarousel({
+    items: 1,
+    margin: 5,
+    loop: true,
+    center: true,
+    nav: true,
+    dots: false,
+  });
+
+  // Residents
+  $('.residents__list').owlCarousel({
+    loop: true,
+    nav: true,
+    // autoWidth: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      1290: {
+        items: 5,
+        margin: 30,
+      }
+    }
+  });
 });
